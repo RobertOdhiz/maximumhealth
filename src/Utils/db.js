634465472b6d75794404b64f92c3cdbc0bc4d375
Robert Odhiambo <odhizrober254@gmail.com>
2024-09-09 23:58:44 +0300

    sorted cors(I think)

--- src/Utils/db.js
+++ src/Utils/db.js
@@ -1,52 +1,41 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://script.google.com/macros/s/AKfycbxotnAcw4IsOudO3ZnroOs_mJigvAM4Xu9f3Zq9kyyTpsZQRlJt-p0kAf96vfsQgi7QmQ/exec';
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbxv8Bladn_zwdo_Sp4T31Awu89osRUtFMtDYSY34r4ew13YV0o-YjzYU5CQohGWWUZkdg/exec';
 
 const request = async (method, endpoint, body = null) => {
-    const options = {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    if (body) {
-      options.body = JSON.stringify(body);
-    }
-    const response = await fetch(`${BASE_URL}${endpoint}`, options);
-    const data = await response.json();
-    return data;
+  const options = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
   };
-  
-  // Function to get data from a sheet
-  export const getData = async (sheetName) => {
-    const endpoint = `?sheet=${sheetName}`;
-    return await request('GET', endpoint);
-  };
-  
-  // Function to create a new record
-  export const createRecord = async (sheetName, data) => {
-    try {
-      const response = await axios.post(`${BASE_URL}?sheet=${sheetName}`, data, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return response;
-    } catch (error) {
-      console.error('Error creating record:', error);
-      throw error;
-    }
-  };
-  
-  // Function to update an existing record
-  export const updateRecord = async (sheetName, data) => {
-    const endpoint = `?sheet=${sheetName}`;
-    return await request('PUT', endpoint, data);
-  };
-  
-  // Function to delete a record
-  export const deleteRecord = async (sheetName, id) => {
-    const endpoint = `?sheet=${sheetName}&id=${id}`;
-    return await request('DELETE', endpoint);
-  };
-  
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+  const response = await fetch(`${BASE_URL}${endpoint}`, options);
+  const data = await response.json();
+  return data;
+};
+
+// Function to get data from a sheet
+export const getData = async (sheetName) => {
+  const endpoint = `?sheet=${sheetName}`;
+  return await request('GET', endpoint);
+};
+
+// Function to create a new record
+export const createRecord = async (sheetName, data) => {
+  const endpoint = `?sheet=${sheetName}`;
+  return await request('POST', endpoint, data);
+};
+
+// Function to update an existing record
+export const updateRecord = async (sheetName, data) => {
+  const endpoint = `?sheet=${sheetName}`;
+  return await request('PUT', endpoint, data);
+};
+
+// Function to delete a record
+export const deleteRecord = async (sheetName, id) => {
+  const endpoint = `?sheet=${sheetName}&uuid=${id}`;
+  return await request('DELETE', endpoint);
+};
